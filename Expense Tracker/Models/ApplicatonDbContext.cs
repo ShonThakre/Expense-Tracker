a1939a5381d58a1d7c087816288b55127d79751f
shon <shoonthakre@gmail.com>
2023-12-13 21:17:29 +0530

    ApplicationDbContext and DbSet added

---
+++ Expense Tracker/Models/ApplicatonDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicatonDbContext:DbContext
+    {
+        public ApplicatonDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Transaction> transactions { get; set; }
+    }
+}
