f8c4198266336778748d53018d75245060f8dd29
shon <shoonthakre@gmail.com>
2024-01-02 01:24:10 +0530

    Dashboard functions added, Dashboard Index view modified, sidebar created.

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -14,7 +14,7 @@ public partial class Transaction
     public int CategoryId { get; set; }
     public Category? Category { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "Please Select a category.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
     public int Amount { get; set; }
 
     [Column(TypeName = "nvarchar(75)")]
